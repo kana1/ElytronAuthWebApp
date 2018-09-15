8838c0da8e5bebf0abf915b51a77e846505d23bb
Indika Liyanage <indika.liyanage@ebuilder.com>
2018-09-15 14:35:53 +0530

    basic login with form

---
+++ src/main/java/lk/inli/elytron/LoginServlet.java
@@ -0,0 +1,43 @@
+package lk.inli.elytron;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginServlet() {
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		if ("elytron".equals(username) && "custom".equals(password)) {
+			response.getWriter().append("Successfully logged in as: ").append(username);
+		} else {
+			response.sendRedirect("/");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
